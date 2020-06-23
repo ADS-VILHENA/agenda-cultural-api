3d03bb508dad474493d5448c97af280436869719
MatheusAnciloto <48607587+MatheusAnciloto@users.noreply.github.com>
2020-06-22 20:31:42 -0400

    Rota de cadastro de organizador

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -6,7 +6,7 @@ module.exports = {
         filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
     },
     migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        directory: path.resolve(__dirname, 'src', 'database', 'we')
     },
     seeds: {
         directory: path.resolve(__dirname, 'src', 'database', 'seeds')
