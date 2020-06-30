9aa5b703ded2f6b3a7e92c8a0d5748438b0726e3
MatheusAnciloto <48607587+MatheusAnciloto@users.noreply.github.com>
2020-06-29 22:11:06 -0400

    adicionando modulo bcrypt para gravar senha

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
+import expressValidator from 'express-validator';
+import bcrypt from 'bcrypt';
 
+const saltRounds = 10;
 const routes = express.Router();
 
 //rota de cadastro de um organizador de eventos da agenda
@@ -12,7 +15,7 @@ routes.post('/organizador', async (request, response) => {
             email,
             senha
         } =  request.body;
-        
+
         //query que busca email informado no banco de dados
         async function queryEmail(){
             return await knex.select('*').from('organizador').where('email', email);
@@ -20,23 +23,53 @@ routes.post('/organizador', async (request, response) => {
 
         const checkMail = await queryEmail();
 
-        //verifica se a query retornou algum valor
+        //verifica se a query retornou algum valor, para que não seja cadastrado um email duplicado
         if (checkMail.length > 0){
-            return response.json({ success: false });
+            return response.json({ create: false });
         }
 
         else{
-            await knex('organizador').insert({
-                nome,
-                telefone,
-                endereco,
-                email,
-                senha
-            });
-                    
-            return response.json({ success: true });
-        }
+            bcrypt.hash(senha, saltRounds, async function(err, hash){
+                const senha = hash;
+                await knex('organizador').insert({
+                    nome,
+                    telefone,
+                    endereco,
+                    email,
+                    senha
+                }); 
+            })
+            return response.json({ create: true });
+        };
 });
+
+
+//rota para login do organizador
+routes.post('/organizador/login', async (request, response) => {
+    const {
+        email,
+        senha
+    } = request.body;
+
+    async function queryLogin(){
+        return await knex.select('id').from('organizador').where({
+            'email': email,
+            'senha': senha
+        });
+    }
+
+    const checkLogin = await queryLogin();
+
+    if(checkLogin.length > 0){
+        return response.json({ login: true });
+    }
+    else{
+        return response.json({ login: false });
+    }
+
+
+});
+
 
 /* routes.get('/categorias', async (request, response) => {
     const categorias = await knex('categorias').select('*');
