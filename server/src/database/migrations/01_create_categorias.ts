6ed87f34508d924d3927a81121133b02e2157936
MatheusAnciloto <48607587+MatheusAnciloto@users.noreply.github.com>
2020-07-29 19:45:28 -0400

    adicionada rota para criação de categoria

--- server/src/database/migrations/01_create_categorias.ts
+++ server/src/database/migrations/01_create_categorias.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex) {
         table.increments('id').primary();
         table.string('nome').notNullable();
         table.string('descricao').notNullable();
-        table.string('image').notNullable();
+        table.string('imagem').notNullable();
     });
 }
 
