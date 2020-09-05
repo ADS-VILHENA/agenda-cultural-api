3c7f4beac19f781866824a49f4c9abe1e5abe693
MatheusAnciloto <48607587+MatheusAnciloto@users.noreply.github.com>
2020-09-04 22:59:51 -0400

    criação de rotas para controle dos eventos

--- src/controllers/eventoController.ts
+++ src/controllers/eventoController.ts
@@ -22,7 +22,7 @@ class EventoController {
                 'descricao': descricao
         })
 
-        if (checkEvento != undefined) {
+        if (checkEvento.length > 0) {
             return response.status(409).send({message: 'evento já está cadastrado'});
         } 
         else {
@@ -50,7 +50,46 @@ class EventoController {
     }
 
     async view(request: Request, response: Response) {
-        const eventos = await knex('eventos').select('*');
+        const { id } = request.params;
+        
+        const eventos = await knex('eventos')
+            .select('titulo', 'descricao')
+            .where('id_categoria', id);
+
+        if (eventos) {
+            return response.status(200).send(eventos);
+        } else {
+            return response.status(500).send({message: 'Ocorreu um erro ao buscar pelos eventos relacionados a essa categoria'});
+        }
+    }
+
+    async viewDetails(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const detalhes = await knex('eventos')
+            .select('nome', '*')
+            .innerJoin('organizador', 'organizador.id', 'eventos.id_organizador')
+            .where('eventos.id', id);
+
+        const serializedEvento = detalhes.map(detalhes => {
+            return {
+                titulo: detalhes.titulo,
+                logo: `http://localhost:3333/uploads/${detalhes.logo}`,
+                descricao: detalhes.descricao,
+                endereco: detalhes.endereco,
+                localizacao: detalhes.localizacao,
+                telefone: detalhes.telefone,
+                data: detalhes.data,
+                organizador: detalhes.nome,
+            };
+        });
+    
+        if(serializedEvento.length > 0){
+            response.status(200).send(serializedEvento);
+        } 
+        else {
+            return response.status(404).send({ message: 'Ops, nada para ver aqui...'})
+        }
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,63 @@ import express from 'express';
 import OrgController from './controllers/organizadorController';
 import CtgController from './controllers/categoriaController';
 import EvtController from './controllers/eventoController';
-import { celebrate, Segments, Joi } from '../node_modules/celebrate';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 
 const routes = express();
 const orgController = new OrgController();
 const ctgController = new CtgController();
 const evtController = new EvtController();
+
+
+//rota para visualização de todas categorias
+routes.get('/', ctgController.view);
+
+//rota para cadastro de categoria
+routes.post('/categoria', ctgController.create);
+
+//rota para alterar categoria
+routes.put('/categoria/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), ctgController.update);
+
+//rota para deletar categoria
+routes.delete('/categoria/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), ctgController.delete);
+
+//rota para visualização dos eventos por categoria
+routes.get('/categoria/:id/eventos', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), evtController.view);
+
+//rota para criação de eventos
+routes.post('/evento', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        titulo: Joi.string().required(),
+        descricao: Joi.string().required(),
+        endereco: Joi.string().required(),
+        localizacao: Joi.string(),
+        telefone: Joi.string().required(),
+        data: Joi.date().required(),
+        logo: Joi.string(),
+        id_categoria: Joi.number().required(),
+        id_organizador: Joi.number().required(),
+    })
+}), evtController.create)
+
+//rota para visualização dos detalhes do evento
+routes.get('/detalhes/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), evtController.viewDetails);
 
 //rota de cadastro de um organizador de eventos da agenda
 routes.post('/organizador', celebrate({
@@ -46,41 +96,9 @@ routes.delete('/organizador/:id', celebrate({
     })
 }), orgController.delete);
 
-//rota para cadastro de categoria
-routes.post('/categoria', ctgController.create);
-
-//rota para visualização de todas categorias
-routes.get('/', ctgController.view);
-
-//rota para alterar categoria
-routes.put('/categoria/:id', celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-        id: Joi.number().required(),
-    })
-}), ctgController.update);
-
-//rota para deletar categoria
-routes.delete('/categoria/:id', celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-        id: Joi.number().required(),
-    })
-}), ctgController.delete);
 
 
-//rota para criação de eventos
-routes.post('evento', celebrate({
-    [Segments.BODY]: Joi.object().keys({
-        titulo: Joi.string().required(),
-        descricao: Joi.string().required(),
-        endereco: Joi.string().required(),
-        localizacao: Joi.string(),
-        telefone: Joi.string().required(),
-        data: Joi.date().required(),
-        logo: Joi.string(),
-        id_categoria: Joi.number().required(),
-        id_organizador: Joi.number().required(),
-    })
-}), evtController.create)
+
 
 
 
